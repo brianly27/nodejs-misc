c76dbf08ef3a7bfecdc98c41bde6c56f29eb9e04
brianly27 <brian.kyang.ly@gmail.com>
2020-07-06 14:47:48 -0700

    compared setImmediate callback to setTimeout() and process.nextTick

--- node-event-loop/nextTick.js
+++ node-event-loop/nextTick.js
@@ -8,12 +8,16 @@ const foo = () => {
     process.nextTick( () => {
         bar()
     })
+    setImmediate( () => {
+        console.log('immediate')
+    })
     console.log('foo')
 }
 
 foo()
 
-//returns: foo, foo, bar, baz
+//returns: foo, foo, bar, baz, immediate
 //nextTick() is called after foo() is finshed being executed 
 //nextTick is called before the setTimeout() callback is executed
+//setImmediate is called in the next loop but after the setTimeout
 
